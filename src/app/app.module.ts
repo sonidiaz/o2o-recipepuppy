f960f29c1c3be355656b893ffc6efc01438f8c36
Ignacio Díaz <nachodiaz8@gmail.com>
2018-10-23 00:59:06 +0200

    Second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { RecetasComponent } from './components/recetas/recetas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    RecetasComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/recetas/recetas.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, Headers } from '@angular/common/http';
+import { RecetasService } from '../../servicios/recetas.service';
+
+
+@Component({
+  selector: 'app-recetas',
+  templateUrl: './recetas.component.html'
+})
+export class RecetasComponent implements OnInit {
+
+  recetas:Recetas[] = [];
+  constructor( private recetasService: RecetasService, private http: HttpClient ) {
+    
+    // this.http.get('http://www.recipepuppy.com/api/').subscribe( receta => console.log(receta) );
+  }
+
+  ngOnInit() {
+    this.recetas = this.recetasService.getRecetas();
+    // console.log(this.recetas);
+  }
+
+}
